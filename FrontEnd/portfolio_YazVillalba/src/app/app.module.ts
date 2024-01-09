ef4437efd4a34b2b99572a5e38b7af3d7d6421d5
Yazvillalba <yazminvillalba55@gmail.com>
2024-01-09 18:14:03 -0300

    FrontEnd del Portfolio

---
+++ FrontEnd/portfolio_YazVillalba/src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAppComponent } from './components/logo-app/logo-app.component';
+import { RedesSocialesComponent } from './components/redes-sociales/redes-sociales.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HysComponent} from './components/hys/hys.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import{HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+// import { LoginComponent } from './components/login/login.component';
+// import { NuevoUsuarioComponent } from './model/nuevo-usuario/nuevo-usuario.component';
+@NgModule({
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        LogoAppComponent,
+        RedesSocialesComponent,
+        BannerComponent,
+        AcercaDeComponent,
+        ExperienciaComponent,
+        EducacionComponent,
+        HysComponent,
+        ProyectosComponent,
+        HomeComponent,
+        FooterComponent
+        // LoginComponent,
+        // NuevoUsuarioComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        NgCircleProgressModule.forRoot({}),
+        HttpClientModule,
+        FormsModule,
+    ]
+})
+export class AppModule { }
---
+++ FrontEnd/portfolio_YazVillalba/src/app/components/logo-app/logo-app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logo-app',
+  templateUrl: './logo-app.component.html',
+  styleUrl: './logo-app.component.css'
+})
+export class LogoAppComponent {
+
+}
